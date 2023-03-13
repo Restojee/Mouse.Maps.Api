0e64962de4847d1d548a24c1ae981b75c4fe2811
Rest <vladafanasevrest@gmail.com>
2023-03-14 00:54:20 +0500

    added: other

---
+++ src/main/java/com/mouse/maps/maps/models/CompletedMap.java
@@ -0,0 +1,14 @@
+package com.mouse.maps.maps.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CompletedMap {
+    public Integer mapId;
+
+    public Integer userId;
+
+    public String image;
+}
